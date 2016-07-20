6a39ac058649e73519a63ad6d05e6f8efed2968a
nafiskarim <sahil.abrar@gmail.com>
2016-07-20 10:53:44 +0200

    some msg modification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article creation is successful"
+      flash[:notice] = "Article is succesfully created"
       redirect_to article_path(@article)
     else
       render 'new'
