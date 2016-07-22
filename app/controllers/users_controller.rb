b2809366a8e366c76fd47385679a10d94843bb16
nafiskarim <sahil.abrar@gmail.com>
2016-07-22 16:00:19 +0200

    adding admin user functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-  	if current_user != @user
+  	if current_user != @user and !current_user.admin?
   		flash[:danger] = "You can edit only your profile"
   		redirect_to root_path
   	end
