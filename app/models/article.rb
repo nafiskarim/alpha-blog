31a7e364212887f22ed7d35bb41f95710b598a48
nafiskarim <sahil.abrar@gmail.com>
2016-07-25 14:30:06 +0200

    article and category assosiation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   #validation for database entry
   validates :title, presence: true, length: { minimum: 3, maximum: 30}
   validates :description, presence: true, length: { minimum: 10, maximum: 200}
